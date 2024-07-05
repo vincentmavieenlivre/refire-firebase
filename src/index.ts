a55116ba9345f04825489c60798b307b9d69f844
Vincent Leyrit <vincent.leyrit@weeakt.com>
2024-07-05 16:55:02 +0200

    feat(initializeApp from firebase)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { initializeApp as initializeFirebase } from "@firebase/app";
+export { initializeApp as initializeFirebase } from 'firebase/app';
 export { FirebaseAuth } from "./firebaseAuth";
 export { FirebaseDatabase } from "./firebaseDatabase";
 export { FirestoreDatabase } from "./FirestoreDatabase";
